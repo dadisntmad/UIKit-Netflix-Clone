d09f837058a64801f858f177cbf882946a51a2d2
Ruslan <ninepexels@gmail.com>
2026-08-10 18:50:59 +0300

    project init

--- NetflixClone/ViewController.swift
+++ NetflixClone/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  NetflixClone
-//
-//  Created by Ruslan Lantsov on 10.08.2026.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
