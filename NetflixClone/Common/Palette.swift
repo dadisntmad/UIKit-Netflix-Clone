8854f4d0c0105885701a83db397d495b664cdbd9
Ruslan <ninepexels@gmail.com>
2026-08-10 19:12:23 +0300

    - creates basic folder structure; - adds logo; - adds colors

---
+++ NetflixClone/Common/Palette.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum Palette {
+    static let accentRed = UIColor(named: "accentRed")
+    static let accentGrey = UIColor(named: "accentGrey")
+    static let primaryBackground = UIColor(named: "primaryBackground")
+    static let secondaryBackground = UIColor(named: "secondaryBackground")
+}
---
+++ NetflixClone/Modules/Auth/AuthViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class AuthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .secondaryBackground
+    }
+}
+
