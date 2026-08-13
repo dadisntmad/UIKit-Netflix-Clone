92da04cf252e67f2848ecebfb133b5d2e8dc8a79
Ruslan <ninepexels@gmail.com>
2026-08-13 20:04:10 +0300

    implements KeychainService

---
+++ NetflixClone/Services/KeychainService.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+protocol KeychainServiceProtocol {
+    func save(_ value: String, forKey key: String) async
+    func load(forKey key: String) async -> String?
+    func delete(forKey key: String)
+}
+
+final class KeychainService: KeychainServiceProtocol {
+    func save(_ value: String, forKey key: String) async {
+        guard let data = value.data(using: .utf8) else { return }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+    
+    func load(forKey key: String) async -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data else {
+            return nil
+        }
+        
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func delete(forKey key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+    }
+}
